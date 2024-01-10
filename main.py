23aac7d184c5eafb618f94437055e4fec566b335
Pierre <pbonnamy.pro@gmail.com>
2024-01-11 00:09:02 +0100

    format

--- main.py
+++ main.py
@@ -14,17 +14,21 @@ MAP_SIZE = 50
 GRASS_COLOR_1 = (189, 244, 101)
 GRASS_COLOR_2 = (181, 236, 93)
 
+
 class GameWindow(arcade.Window):
     def __init__(self):
-        super().__init__(MAP_COL*MAP_SIZE, MAP_ROW*MAP_SIZE, 'Crossy road')
+        super().__init__(MAP_COL * MAP_SIZE, MAP_ROW * MAP_SIZE, 'Crossy road')
 
     def on_draw(self):
         arcade.start_render()
         for row in range(MAP_ROW):
             if row % 2 == 0:
-                arcade.draw_rectangle_filled(MAP_COL*MAP_SIZE/2, MAP_SIZE/2 + row*MAP_SIZE, MAP_COL*MAP_SIZE, MAP_SIZE, GRASS_COLOR_1)
+                arcade.draw_rectangle_filled(MAP_COL * MAP_SIZE / 2, MAP_SIZE / 2 + row * MAP_SIZE, MAP_COL * MAP_SIZE,
+                                             MAP_SIZE, GRASS_COLOR_1)
             else:
-                arcade.draw_rectangle_filled(MAP_COL*MAP_SIZE/2, MAP_SIZE/2 + row*MAP_SIZE, MAP_COL*MAP_SIZE, MAP_SIZE, GRASS_COLOR_2)
+                arcade.draw_rectangle_filled(MAP_COL * MAP_SIZE / 2, MAP_SIZE / 2 + row * MAP_SIZE, MAP_COL * MAP_SIZE,
+                                             MAP_SIZE, GRASS_COLOR_2)
+
 
 if __name__ == '__main__':
     window = GameWindow()
