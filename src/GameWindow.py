6a13e279b889302848380d476293522124fdf8ee
noeoxycode <76428537+noeoxycode@users.noreply.github.com>
2024-02-10 19:21:22 +0100

    map to string ok get positiion ok get radar ok

--- src/GameWindow.py
+++ src/GameWindow.py
@@ -20,6 +20,14 @@ REWARD_GOAL = 100
 REWARD_WALL = -100
 
 
+MAP_START = 'S'
+MAP_GOAL = 'A'
+MAP_WALL = 'O'
+
+
+def sign(x):
+    return 1 if x > 0 else -1 if x < 0 else 0
+
 class GameWindow(arcade.Window):
     def __init__(self, debug_mode):
         super().__init__(MAP_COL * SPRITE_SIZE, MAP_ROW * SPRITE_SIZE, 'Crossy Road')
@@ -54,6 +62,38 @@ class GameWindow(arcade.Window):
 
             self.lanes.append(lane)
 
+    def get_pos(self):
+        return self.player.current_row(), self.player.current_col()
+
+    def map_to_string(self):
+        player_row, player_col = self.get_pos()
+        map_str = ""
+        for index, lane in enumerate(self.lanes):
+            if isinstance(lane, SafeZone):
+                if index == MAP_ROW - 1:
+                    map_str += "A" * MAP_COL  # 'A' représente l'arrivée
+                else:
+                    map_str += "S" * MAP_COL  # 'S' représente une la SafeZone de départ
+            elif isinstance(lane, Grass):
+                lane_str = ["."] * MAP_COL  # '.' représente un espace vide
+                for obstacle in lane.obstacles:
+                    col = int(obstacle.center_x / SPRITE_SIZE)  # Calculer la colonne de l'obstacle
+                    lane_str[col] = "O"  # 'O' représente un obstacle
+                map_str += "".join(lane_str)
+            elif isinstance(lane, Road):
+                lane_str = ["."] * MAP_COL  # '.' représente un espace vide
+                for car in lane.cars:
+                    col = int(car.center_x / SPRITE_SIZE)  # Calculer la colonne de la voiture
+                    lane_str[col] = "C"  # 'C' représente une voiture
+                map_str += "".join(lane_str)
+            map_str += "\n"  # Nouvelle ligne à la fin de chaque lane
+
+        # Remplacement de l'élément correspondant à la position du joueur par une autre lettre
+        map_str = map_str[:player_row * (MAP_COL + 1) + player_col] + "P" + map_str[player_row * (
+                    MAP_COL + 1) + player_col + 1:]
+
+        return map_str
+
     def on_draw(self):
         arcade.start_render()
 
@@ -69,6 +109,25 @@ class GameWindow(arcade.Window):
                          bold=True)
         arcade.draw_text('Losses: ' + str(self.loss_count), 5, MAP_ROW * SPRITE_SIZE - 40, arcade.color.BLACK, 14,
                          bold=True)
+
+    def get_radar(self, state):
+        row, col = state[0], state[1]
+        neighbors = [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1),
+                     (row - 2, col), (row + 2, col), (row, col - 2), (row, col + 2)]
+        radar = []
+        for n in neighbors:
+            if n in self.map:
+                radar.append(self.map[n])
+            else:
+                radar.append(MAP_WALL)
+        delta_row = sign(self.goal[0] - row) + 1
+        delta_col = sign(self.goal[1] - col) + 1
+        radar_goal = [0] * 9
+
+        position = delta_row * 3 + delta_col
+        radar_goal[position] = 1
+
+        return tuple(radar + radar_goal)
 
     def on_update(self, delta_time):
         print(self.map_to_string())
@@ -95,6 +154,7 @@ class GameWindow(arcade.Window):
                 if lane.hit_by_car(self.player):
                     self.loss_count += 1
                     self.player.reset_position()
+        return self.get_radar(self.get_pos())
 
     def on_key_press(self, key, modifiers):
         self.player.move(key, self.lanes)
